296b8bc14f38ddb9ff0f3ac38a577636305917a1
Vicente Alencar <vicente.alencar@gmail.com>
2019-05-08 23:15:55 -0300

    Implement block exceptions

--- ulysses.py
+++ ulysses.py
@@ -1,11 +1,45 @@
+import json
 import os
 import socket
+
+from datetime import datetime
+from dateutil import rrule
+from dateutil.parser import parse
+from recurrent import RecurringEvent
+
+def is_day_in_recurrence(recurrence_string, day):
+    recurrence = rrule.rrulestr(RecurringEvent().parse(recurrence_string))
+    return (recurrence[0] - day).days == 0
+
+def is_time_in_interval(begin_str, end_str, time):
+    begin_time = parse(begin_str).time()
+    end_time = parse(begin_str).time()
+
+    return begin_time <= time and time <= end_time
 
 with open("/etc/pf.conf", "r") as etc_pf:
     pf_conf = etc_pf.read()
 
 with open("./block_list.txt", "r") as block_list_file:
     block_list = [s.strip() for s in block_list_file.readlines()]
+
+with open("./exceptions.json") as exceptions_file:
+    block_exceptions = json.loads(exceptions_file.read())
+
+for be in block_exceptions:
+    today = datetime.now()
+
+    if not is_day_in_recurrence(be["frequency"], today):
+        continue
+
+    if reduce(operator.and_, 
+            map(lambda period: is_time_in_interval(period[0], period[1], today.time()), be['time_periods'])):
+
+        block_list = list(set(block_list) - set(be['urls']))
+
+# TODO: test code above
+
+
 
 ips_to_block = []
 for host_name in block_list:
