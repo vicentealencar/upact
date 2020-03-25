58b3723e233d1a30f5da938c8ffb62225ac8d26f
Vicente Alencar <vicente.alencar@gmail.com>
2020-03-25 16:36:59 -0300

    Refactoring configuration files

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
 import os
 
-FOLDER = "./sample"
+DATA_FOLDER = "./data"
+CONFIG_FOLDER = os.path.join(DATA_FOLDER, "config")
+STATIC_FOLDER = os.path.join(DATA_FOLDER, "static")
+AUTO_GENERATED_FOLDER = os.path.join(DATA_FOLDER, "auto_generated")
+
+# Configuratios
+ALLOWED_DNSES_FILE = os.path.join(CONFIG_FOLDER, "nameservers-allowed.txt")
+URLS_TO_BLOCK = os.path.join(CONFIG_FOLDER, "block_list.txt")
+PORTS_TO_BLOCK = os.path.join(CONFIG_FOLDER, "block_ports.txt")
+PLAYTIME_RULES = os.path.join(CONFIG_FOLDER, "playtime_rules.json")
+
+# Static
+PF_CONF_TEMPLATE = os.path.join(STATIC_FOLDER, "conf_template.conf")
+
+# URLS
 ALL_DNSES_URL = "https://public-dns.info/nameservers.txt"
-ALL_DNSES_FILE = os.path.join(FOLDER, "nameservers-all.txt")
-ALLOWED_DNSES_FILE = os.path.join(FOLDER, "nameservers-allowed.txt")
-PF_CONF_PATH = os.path.join(FOLDER, "pf.conf")
-PF_CONF_TEMPLATE = os.path.join(FOLDER, "conf_template.conf")
 INTERNET_CONNECTIVITY_URL = "www.google.com"
-URLS_TO_BLOCK = os.path.join(FOLDER, "block_list.txt")
-PORTS_TO_BLOCK = os.path.join(FOLDER, "block_ports.txt")
-PLAYTIME_RULES = os.path.join(FOLDER, "playtime_rules.json")
+
+# Auto generated files
+ALL_DNSES_FILE = os.path.join(AUTO_GENERATED_FOLDER, "nameservers-all.txt")
+PF_CONF_PATH = os.path.join(AUTO_GENERATED_FOLDER, "pf.conf")
