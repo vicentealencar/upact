796c518a4eb1bdd0a18074e1c443693a4e6f821e
Vicente Alencar <vicente.alencar@gmail.com>
2021-12-01 15:39:16 -0300

    Small fix to config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_FOLDER = "../data"
+DATA_FOLDER = "data"
 CONFIG_FOLDER = os.path.join(DATA_FOLDER, "private")
 STATIC_FOLDER = os.path.join(DATA_FOLDER, "static")
 AUTO_GENERATED_FOLDER = os.path.join(DATA_FOLDER, "auto_generated")
@@ -26,3 +26,4 @@ ALL_DNSES_FILE = os.path.join(AUTO_GENERATED_FOLDER, "nameservers-all.txt")
 PF_CONF_PATH = os.path.join(AUTO_GENERATED_FOLDER, "pf.conf")
 STAGING_URLS_TO_BLOCK = os.path.join(AUTO_GENERATED_FOLDER, "staging_urls_to_block.jsonl")
 BLOCKED_IPS_FILE = os.path.join(AUTO_GENERATED_FOLDER, "blocked_ips.txt")
+BLOCKED_IPS_JSON = os.path.join(AUTO_GENERATED_FOLDER, "blocked_ips.json")
