e6f686837ee2fdc007cddd78c037c4049d6e7f2c
Vicente Alencar <vicente.alencar@gmail.com>
2021-12-06 20:14:41 -0300

    Using new database structure for the app fence
    Also refactoring the app fence.

--- src/app_fence.py
+++ src/app_fence.py
@@ -1,43 +1,26 @@
-import json
-import operator
 import subprocess
 import logging
-
 import config
 
-from upact.datetime import is_time_in_interval, is_day_in_recurrence
+import peewee as pw
 
-from datetime import datetime
-from functools import reduce
-
-
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+from upact.models import *
 
 
-logging.info("Running upact app")
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
 
-with open(config.APPS_TO_BLOCK, "r") as block_list_file:
-    block_list = [s.strip() for s in block_list_file.readlines()]
+    logging.info("Running upact app")
 
-with open(config.APP_PLAYTIME_RULES) as exceptions_file:
-    block_exceptions = json.loads(exceptions_file.read())
+    db = pw.SqliteDatabase(config.DATABASE_FILE)
+    db.connect()
+    database_proxy.initialize(db)
 
-for be in block_exceptions:
-    today = datetime.now()
+    apps_to_block = [uri.name for uri in Uri.select().where(Uri.type_uri == 'application') if not(any([playtime.is_active() for playtime in uri.playtime_rules]))]
 
-    if not is_day_in_recurrence(be["frequency"], today):
-        continue
-
-    if reduce(operator.or_, 
-            map(lambda period: is_time_in_interval(period[0], period[1], today.time()), be['time_periods'])):
-
-        print("The APP(s) %s are currently accessible. Rule: %s" % (be['apps'], be))
-
-        block_list = list(set(block_list) - set(be['apps']))
-
-for app_to_kill in block_list:
-    logging.info(f"Killing {app_to_kill}")
-    subprocess.call(["sudo", "pkill", "-9", app_to_kill])
+    for app_to_kill in apps_to_block:
+        logging.info(f"Killing {app_to_kill}")
+        subprocess.call(["sudo", "pkill", "-9", app_to_kill])
