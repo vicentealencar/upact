08c89e0e0ac591591aae027f8ab76b9033caef95
Vicente Alencar Jr <vicente.alencar@gmail.com>
2022-08-08 14:15:30 -0300

    Moving the command logic under the cmdline submodule

--- src/command.py
+++ src/command.py
@@ -1,23 +1,5 @@
-import argparse
-import config
-
-import peewee as pw
-
-from upact.cmdline.subcommands import urls
-from upact.models import database_proxy
+from upact.cmdline import command
 
 
 if __name__ == "__main__":
-    db = pw.SqliteDatabase(config.DATABASE_FILE)
-    db.connect()
-    database_proxy.initialize(db)
-
-    main_parser = argparse.ArgumentParser()
-    subparsers = main_parser.add_subparsers()
-
-    urls.sub_parser(subparsers)
-
-    parsed_args = main_parser.parse_args()
-
-    if getattr(parsed_args, 'init_command', None):
-        parsed_args.init_command(parsed_args)()
+    command.run()
