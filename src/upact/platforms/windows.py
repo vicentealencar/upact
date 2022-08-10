784ffed68841dfc3c57e31a2b8d5658b5b1ccab2
Vicente Alencar Jr <vicente.alencar@gmail.com>
2022-08-10 19:36:22 -0300

    Updating windows platform to create a single firewall rule

--- src/upact/platforms/windows.py
+++ src/upact/platforms/windows.py
@@ -3,47 +3,32 @@ import logging
 
 class Windows:
 
-    RULE_NAME_PREFIX = "upact"
+    def _write(self, stream, blob_str):
+        logging.info(blob_str)
+        stream.write(bytes(blob_str, 'utf-8'))
 
-    def _rule_name(self, ip):
-        return f"{self.RULE_NAME_PREFIX} {ip}"
+    def _block_ips(self, ip_addresses, rule_name):
+        with subprocess.Popen("powershell.exe", stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) as process:
+            self._write(process.stdin, "$ips_to_block = @()\n")
+            for ip in ip_addresses:
+                self._write(process.stdin, f'$ips_to_block += "{ip.address}"\n')
 
-    def _block_ip(self, ip):
-        add_rule_command = f'netsh advfirewall firewall add rule name="{self._rule_name(ip)}" dir=out action=block remoteip={ip}'
-        logging.info(add_rule_command)
-        subprocess.call(add_rule_command)
+            self._write(process.stdin, f'New-NetFirewallRule -DisplayName "{rule_name}" -Direction Outbound -Action Block -RemoteAddress $ips_to_block\n')
+            process.stdin.flush()
+
 
     def _remove_rule(self, rule_name):
         remove_rule_command = f'netsh advfirewall firewall delete rule name="{rule_name}"'
         logging.info(remove_rule_command)
         subprocess.call(remove_rule_command)
 
-    def _raw_firewall_rules_output(self, rule_prefix):
-        command = f"(New-object -comObject HNetCfg.FwPolicy2).rules | select name | findstr /i '{rule_prefix}'"
-        try:
-            logging.info(f"Running powershell command {command}")
-            return subprocess.check_output(["powershell.exe", 
-                                            "-Command",
-                                            command], 
-                                        shell=True)
-        except subprocess.CalledProcessError as ex:
-            logging.info(f"Exception from powershell command {command}")
-            return b''
-
-    def _parse_firewall_rules(self, raw_output):
-        return [s.strip() for s in raw_output.decode('utf-8').strip().split("\r\n") if s]
-
-    def _list_firewall_rules_names(self):
-        return self._parse_firewall_rules(self._raw_firewall_rules_output(self.RULE_NAME_PREFIX))
-
-    def update_firewall(self, ips_to_block, ips_to_unblock, config):
+    def update_firewall(self, ips_to_block, ips_to_unblock, config, rule_name="upact blocked ips"):
 
         logging.info("Removing existing rules...")
-        for rule_name in self._list_firewall_rules_names():
-            self._remove_rule(rule_name)
+        self._remove_rule(rule_name)
 
-        logging.info("Adding new rules...")
-        for ip in ips_to_block:
-            self._block_ip(ip.address)
+        if ips_to_block:
+            logging.info("Adding new rules...")
+            self._block_ips(ips_to_block, rule_name)
 
         logging.info("Done updating blocked ips")
--- tests/platforms/windows_tests.py
+++ tests/platforms/windows_tests.py
@@ -8,45 +8,38 @@ from upact.platforms.windows import Windows
 
 class WindowsPlatformTests(TestCase):
 
-    def _verify_rules_removed(self, windows, call_mock, check_output_mock, ips_to_block=[]):
-        check_output_mock.return_value = b'upact 151.101.1.140                                                                                                                                                                  \r\nupact 151.101.129.140                                                                                                                                                                \r\n'
-        windows.update_firewall(ips_to_block=ips_to_block, ips_to_unblock=[], config=None)
+    def _verify_rules_removed(self, windows, call_mock, ips_to_block=[]):
+        windows.update_firewall(ips_to_block=ips_to_block, ips_to_unblock=[], config=None, rule_name="upact test")
 
-        self.assertEqual(call_mock.call_args_list[0].args[0], 'netsh advfirewall firewall delete rule name="upact 151.101.1.140"')
-        self.assertEqual(call_mock.call_args_list[1].args[0], 'netsh advfirewall firewall delete rule name="upact 151.101.129.140"')
+        self.assertEqual(call_mock.call_args_list[0].args[0], 'netsh advfirewall firewall delete rule name="upact test"')
 
-    @patch("subprocess.check_output")
+    @patch("subprocess.Popen")
     @patch("subprocess.call")
-    def test_existing_rules_removed(self, call_mock, check_output_mock):
+    def test_existing_rules_removed(self, call_mock, popen_mock):
 
         windows = Windows()
 
-        self._verify_rules_removed(windows, call_mock, check_output_mock)
+        self._verify_rules_removed(windows, call_mock)
+
+        self.assertEqual(popen_mock.call_count, 0)
 
 
-    @patch("subprocess.check_output")
+    @patch("subprocess.Popen")
     @patch("subprocess.call")
-    def test_rules_removed_and_added(self, call_mock, check_output_mock):
+    def test_rules_removed_and_added(self, call_mock, popen):
 
         windows = Windows()
+
+        process_mock = Mock()
+        popen().__enter__.return_value = process_mock
 
         self._verify_rules_removed(windows,
                 call_mock,
-                check_output_mock,
                 ips_to_block=[BlockedIp(address='200.253.245.1'), BlockedIp(address='200.253.236.1')])
 
-        self.assertEqual(call_mock.call_args_list[2].args[0],
-            'netsh advfirewall firewall add rule name="upact 200.253.245.1" dir=out action=block remoteip=200.253.245.1')
-        self.assertEqual(call_mock.call_args_list[3].args[0],
-                'netsh advfirewall firewall add rule name="upact 200.253.236.1" dir=out action=block remoteip=200.253.236.1')
+        write_mock = process_mock.stdin.write
 
-    @patch("subprocess.check_output")
-    @patch("subprocess.call")
-    def test_no_rules_removed(self, call_mock, check_output_mock):
-
-        windows = Windows()
-
-        check_output_mock.side_effect = CalledProcessError(1, "")
-        windows.update_firewall(ips_to_block=[], ips_to_unblock=[], config=None)
-
-        self.assertEqual(call_mock.call_count, 0)
+        self.assertEqual(write_mock.call_args_list[0][0][0], b'$ips_to_block = @()\n')
+        self.assertEqual(write_mock.call_args_list[1][0][0], b'$ips_to_block += "200.253.245.1"\n')
+        self.assertEqual(write_mock.call_args_list[2][0][0], b'$ips_to_block += "200.253.236.1"\n')
+        self.assertEqual(write_mock.call_args_list[3][0][0], b'New-NetFirewallRule -DisplayName "upact test" -Direction Outbound -Action Block -RemoteAddress $ips_to_block\n')
