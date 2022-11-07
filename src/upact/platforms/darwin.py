f07766d34bf51b7e0303bb9a571cf92ba203037b
Vicente Alencar <vicente.alencar@gmail.com>
2022-11-06 22:48:22 -0300

    Updating Darwin platform to reflect recent changes

--- src/upact/platforms/darwin.py
+++ src/upact/platforms/darwin.py
@@ -4,22 +4,35 @@ import subprocess
 from upact.platforms.base_platform import BasePlatform
 
 class Darwin(BasePlatform):
-    def update_firewall(self, ips_to_block, ips_to_unblock, config):
-        ip_addresses = {ip.address for ip in ips_to_block}
+    
+    def __init__(self, config):
+        self.ips_to_block = set()
+        self.config = config
 
-        with open(config.PF_CONF_TEMPLATE, "r") as etc_pf:
+    def update_permanently_blocked_ips(self, ips_to_block):
+        self.ips_to_block.update([ip.address for ip in ips_to_block])
+        self.update_firewall()
+
+    def update_ips_from_urls(self, ips_to_block, ips_to_unblock):
+        self.ips_to_block.update([ip.address for ip in ips_to_block])
+        self.update_firewall()
+
+    def update_firewall(self):
+        ip_addresses = self.ips_to_block
+
+        with open(self.config.PF_CONF_TEMPLATE, "r") as etc_pf:
             pf_conf = etc_pf.read()
 
-        with open(config.BLOCKED_IPS_FILE, "w") as blocked_ips_file:
+        with open(self.config.BLOCKED_IPS_FILE, "w") as blocked_ips_file:
             blocked_ips_file.write("\n".join(ip_addresses))
 
-        pf_conf += 'table <blocked_ips> persist file "{0}"'.format(os.path.join(os.getcwd(), config.BLOCKED_IPS_FILE))
+        pf_conf += 'table <blocked_ips> persist file "{0}"'.format(os.path.join(os.getcwd(), self.config.BLOCKED_IPS_FILE))
         pf_conf += "\n"
 
         pf_conf += "block return from any to <blocked_ips>"
         pf_conf += "\n"
 
-        with open(config.PF_CONF_PATH, "w+") as pf_conf_file:
+        with open(self.config.PF_CONF_PATH, "w+") as pf_conf_file:
             pf_conf_file.write(pf_conf)
 
-        subprocess.call(["sudo", "pfctl", "-E", "-f", config.PF_CONF_PATH])
+        subprocess.call(["sudo", "pfctl", "-E", "-f", self.config.PF_CONF_PATH])
